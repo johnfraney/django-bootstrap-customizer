2e19b506fcb446bf192097685a7806cc8b40d3f3
John Franey <johnfraney@gmail.com>
2019-09-27 00:12:43 -0400

    Update Bootstrap to 4.3.1 and bump to 0.2.0
    - Updates example themes with Bootstrap 4.3.1
    - Updates supported Django versions
    - Updates Travis to use tox-travis which reuses tox.ini
    - Updates changelog
    - Bumps version

--- HISTORY.md
+++ HISTORY.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.0] - 2019-10-02
+### Changed
+- Updated to Bootstrap 4.3.1
+- Updated Tox configuration
+- Simplified Travis configuration
+
 ## [0.1.4] - 2019-04-18
 ### Fixed
 - Fix an error in Windows caused by mixed slashes in SCSS paths (#2) - @vincent-picot
--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ setup(
     zip_safe=False,
     keywords='django-bootstrap-customizer',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -80,5 +80,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
